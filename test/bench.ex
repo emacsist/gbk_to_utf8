94b304e4cacb0963cf099d590c438f9fb735df1a
emacsist <emacsist@qq.com>
2021-09-18 15:59:50 +0800

    refactor: remove iconv

--- test/bench.ex
+++ test/bench.ex
@@ -1,15 +1,15 @@
-:application.start(:iconv)
+# :application.start(:iconv)
 
-GbkToUtf8Agent.start_link()
+# GbkToUtf8Agent.start_link()
 
-GbkToUtf8.start()
-#gbk_bin = File.read!("hello.gbk.txt") |> IO.inspect()
-gbk_bin = File.read!("6v.gbk.html")
+# GbkToUtf8.start()
+# #gbk_bin = File.read!("hello.gbk.txt") |> IO.inspect()
+# gbk_bin = File.read!("6v.gbk.html")
 
-GbkToUtf8.get_utf_by_gbk_string(gbk_bin)
-:iconv.convert("gbk", "utf-8", gbk_bin)
+# GbkToUtf8.get_utf_by_gbk_string(gbk_bin)
+# :iconv.convert("gbk", "utf-8", gbk_bin)
 
-Benchee.run(%{
-  "my gbk" => fn -> GbkToUtf8.get_utf_by_gbk_string(gbk_bin) end,
-  "iconv gbk" => fn -> :iconv.convert("gbk", "utf-8", gbk_bin) end
-})
+# Benchee.run(%{
+#   "my gbk" => fn -> GbkToUtf8.get_utf_by_gbk_string(gbk_bin) end,
+#   "iconv gbk" => fn -> :iconv.convert("gbk", "utf-8", gbk_bin) end
+# })
